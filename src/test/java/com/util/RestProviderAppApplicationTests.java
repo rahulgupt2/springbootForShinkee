b6af2cf5caab09ab16f955a2e15f1599773576d0
Rahul Gupta <rahul.gupta@sigma-systems.com>
2016-07-12 09:54:30 +0530

    initailize springboot repo

---
+++ src/test/java/com/util/RestProviderAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RestProviderAppApplication.class)
+public class RestProviderAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
